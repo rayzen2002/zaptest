ff1baa852a37c9154e0706159f74e6e368dda087
rayzen2002 <emanuelzsmj@gmail.com>
2025-07-25 10:58:08 -0300

    Testing response

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 // Import Express.js
+const axios = require('axios');
 const express = require('express');
 
 // Create an Express app
@@ -24,11 +25,43 @@ app.get('/', (req, res) => {
 });
 
 // Route for POST requests
-app.post('/', (req, res) => {
-  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+app.post('/', async(req, res) => {
+  const body = req.body
+
+  if (
+    body.object &&
+    body.entry &&
+    body.entry[0].changes &&
+    body.entry[0].changes[0].value.messages &&
+    body.entry[0].changes[0].value.messages[0]
+  ){
+    const message = body.entry[0].changes[0].value.messages[0]
+    const from = message.from
+    const text = message.text?.body || ''
+
+    console.log(`Mensagem recebida de ${from} : ${text}`)
+
+    try {
+      const response = await axios.post(
+        `https://graph.facebook.com/v19.0/${phoneNumberId}/messages`
+      , {
+        messaging_product: 'whatsapp',
+        to: from,
+        text: {
+          body: `Olá! Eu sou um vendedor da NGS`
+        }
+      }, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+           'Content-Type': 'application/json',
+        }
+      })
+      console.log("Mensagem enviada com sucesso!")
+    } catch (error) {
+      console.error('Erro ao enviar resposta:', error.response?.data || error.message);
+    }
+  }
+  res.sendStatus(200)
 });
 
 // Start the server
